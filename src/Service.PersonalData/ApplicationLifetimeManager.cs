ff376a4b7dabfafa4e5406f8515989a32d614528
alexey <alexey.n@smplt.net>
2021-10-18 12:46:25 +0300

    update ServiceBus SDK

--- src/Service.PersonalData/ApplicationLifetimeManager.cs
+++ src/Service.PersonalData/ApplicationLifetimeManager.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MyJetWallet.Sdk.Service;
+using MyJetWallet.Sdk.ServiceBus;
 using MyServiceBus.TcpClient;
 using Service.PersonalData.Services;
 
@@ -9,10 +10,14 @@ namespace Service.PersonalData
     public class ApplicationLifetimeManager : ApplicationLifetimeManagerBase
     {
         private readonly ILogger<ApplicationLifetimeManager> _logger;
-        private readonly MyServiceBusTcpClient _myServiceBusTcpClient;
+        private readonly ServiceBusLifeTime _myServiceBusTcpClient;
         private readonly PersonalDataCache _personalDataCache;
 
-        public ApplicationLifetimeManager(IHostApplicationLifetime appLifetime, ILogger<ApplicationLifetimeManager> logger, MyServiceBusTcpClient myServiceBusTcpClient, PersonalDataCache personalDataCache)
+        public ApplicationLifetimeManager(
+            IHostApplicationLifetime appLifetime, 
+            ILogger<ApplicationLifetimeManager> logger, 
+            ServiceBusLifeTime myServiceBusTcpClient, 
+            PersonalDataCache personalDataCache)
             : base(appLifetime)
         {
             _logger = logger;
