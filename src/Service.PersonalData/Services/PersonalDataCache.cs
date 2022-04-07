4a53d35df552346898f7b76b7388e9fa9dc9fbb2
Alexey Novichikhin <alexey.n@smplt.net>
2022-04-08 01:17:33 +0300

    Add Search PD by names

--- src/Service.PersonalData/Services/PersonalDataCache.cs
+++ src/Service.PersonalData/Services/PersonalDataCache.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,8 +10,9 @@ namespace Service.PersonalData.Services
 {
     public class PersonalDataCache 
     {
-        private readonly Dictionary<string, string> _userIDsByPhone = new();
-        private readonly Dictionary<string, string> _userIDsByEmail = new();
+        private readonly Dictionary<string, List<string>> _userIDsByPhone = new();
+        private readonly Dictionary<string, List<string>> _userIDsByEmail = new();
+        private readonly Dictionary<string, List<string>> _userIDsByNames = new();
         private readonly ILogger<PersonalDataCache> _logger;
 
         private readonly PersonalDataPostgresRepository _personalDataPostgresRepository;
@@ -41,10 +43,26 @@ namespace Service.PersonalData.Services
 
             foreach (var entity in entities)
             {
-                if(!string.IsNullOrEmpty(entity.Email))
-                    _userIDsByEmail.TryAdd(entity.Email, entity.Id);
-                if(!string.IsNullOrEmpty(entity.Phone))
-                    _userIDsByPhone.TryAdd(entity.Phone, entity.Id);
+                if (!string.IsNullOrEmpty(entity.Email))
+                {
+                    if (!_userIDsByEmail.ContainsKey(entity.Email))
+                        _userIDsByEmail[entity.Email] = new List<string>();
+                    _userIDsByEmail[entity.Email].Add(entity.Id);
+                }
+                if (!string.IsNullOrEmpty(entity.Phone))
+                {
+                    if (!_userIDsByPhone.ContainsKey(entity.Phone))
+                        _userIDsByPhone[entity.Phone] = new List<string>();
+                    _userIDsByPhone[entity.Phone].Add(entity.Id);
+                }
+                if (!string.IsNullOrEmpty(entity.FirstName) || !string.IsNullOrEmpty(entity.LastName))
+                {
+                    var names = $"{entity.FirstName ?? ""} {entity.LastName ?? ""}";
+                    
+                    if (!_userIDsByNames.ContainsKey(names))
+                        _userIDsByNames[names] = new List<string>();
+                    _userIDsByNames[names].Add(entity.Id);
+                }
             }
             
             _logger.LogInformation("Initial PD cache population finished");
@@ -52,30 +70,89 @@ namespace Service.PersonalData.Services
 
         public string GetUserIdByPhone(string phone)
         {
-            _userIDsByPhone.TryGetValue(phone, out var id);
-            return id;
+            if (_userIDsByPhone.TryGetValue(phone, out var ids))
+            {
+                if (ids.Count() > 1)
+                {
+                    _logger.LogError($"Cannot get PersonalData by phone {phone}, because exist {ids.Count()} records");
+                    throw new Exception($"Cannot get PersonalData by phone, because exist {ids.Count()} records");
+                }
+
+                return ids.FirstOrDefault();
+            }
+            
+            return string.Empty;
         }
         
         public string GetUserIdByEmail(string email)
         {
-            _userIDsByEmail.TryGetValue(email, out var id);
-            return id;
+            if (_userIDsByEmail.TryGetValue(email, out var ids))
+            {
+                if (ids.Count() > 1)
+                {
+                    _logger.LogError($"Cannot get PersonalData by email {email}, because exist {ids.Count()} records");
+                    throw new Exception($"Cannot get PersonalData by email, because exist {ids.Count()} records");
+                }
+
+                return ids.FirstOrDefault();
+            }
+            
+            return string.Empty;
         }
 
         public void UpdateCache(IPersonalData personalData)
         {
             _logger.LogInformation("Updating PD cache");
-            if(!string.IsNullOrEmpty(personalData.Email) && !_userIDsByEmail.TryAdd(personalData.Email, personalData.Id)) 
-                _userIDsByEmail[personalData.Email] = personalData.Id;
-            if(!string.IsNullOrEmpty(personalData.Phone) && !_userIDsByPhone.TryAdd(personalData.Phone, personalData.Id))
-                _userIDsByPhone[personalData.Phone] = personalData.Id;
+            
+            foreach (var item in _userIDsByPhone)
+            {
+                item.Value.Remove(personalData.Id);
+            }
+            foreach (var item in _userIDsByEmail)
+            {
+                item.Value.Remove(personalData.Id);
+            }
+            foreach (var item in _userIDsByNames)
+            {
+                item.Value.Remove(personalData.Id);
+            }
+            
+            if (!string.IsNullOrEmpty(personalData.Email))
+            {
+                if (!_userIDsByEmail.ContainsKey(personalData.Email))
+                    _userIDsByEmail[personalData.Email] = new List<string>();
+                
+                if (!_userIDsByEmail[personalData.Email].Contains(personalData.Id))
+                    _userIDsByEmail[personalData.Email].Add(personalData.Id);
+            }
+            
+            if (!string.IsNullOrEmpty(personalData.Phone))
+            {
+                if (!_userIDsByPhone.ContainsKey(personalData.Phone))
+                    _userIDsByPhone[personalData.Phone] = new List<string>();
+                
+                if (!_userIDsByPhone[personalData.Phone].Contains(personalData.Id))
+                    _userIDsByPhone[personalData.Phone].Add(personalData.Id);
+            }
+            
+            var names = $"{personalData.FirstName ?? ""} {personalData.LastName ?? ""}";
+            if (!string.IsNullOrEmpty(names))
+            {
+                if (!_userIDsByNames.ContainsKey(names))
+                    _userIDsByNames[names] = new List<string>();
+                
+                if (!_userIDsByNames[names].Contains(personalData.Id))
+                    _userIDsByNames[names].Add(personalData.Id);
+            }
         }
 
         public List<string> Search(string searchText)
         {
-            var resultEmail = _userIDsByEmail.Where(t => t.Key.Contains(searchText)).Select(t => t.Value).ToList();
-            var resultPhone = _userIDsByPhone.Where(t => t.Key.Contains(searchText)).Select(t => t.Value).ToList();
-            return resultEmail.Concat(resultPhone).ToList();
+            var resultEmail = _userIDsByEmail.Where(t => t.Key.Contains(searchText)).SelectMany(t => t.Value).ToList();
+            var resultPhone = _userIDsByPhone.Where(t => t.Key.Contains(searchText)).SelectMany(t => t.Value).ToList();
+            var resultNames = _userIDsByNames.Where(t => t.Key.Contains(searchText)).SelectMany(t => t.Value).ToList();
+            
+            return resultEmail.Concat(resultPhone).Concat(resultNames).Distinct().OrderBy(e => e).ToList();
         }
     }
 }
